24fbe9344658ea1a29214d3322b989eccceec584
Mitch Seymour <mitchseymour@gmail.com>
2018-12-12 19:03:10 -0500

    Fix indentation

--- src/main/resources/archetype-resources/src/main/java/StddevPopUdaf.java
+++ src/main/resources/archetype-resources/src/main/java/StddevPopUdaf.java
@@ -8,48 +8,48 @@ import io.confluent.ksql.function.udaf.UdafFactory;
 public class StddevPopUdaf {
 
     @UdafFactory(description = "computes the population standard deviation for doubles")
-        // Can be used with table aggregations
-        public static TableUdaf<Double, Double> createStddevDouble() {
+    // Can be used with table aggregations
+    public static TableUdaf<Double, Double> createStddevForTables() {
 
-            return new TableUdaf<Double, Double>() {
-                private Long sampleSize = 0L;
-                private Double sum = 0.0;
-                private Double sumSquares = 0.0;
+        return new TableUdaf<Double, Double>() {
+            private Long sampleSize = 0L;
+            private Double sum = 0.0;
+            private Double sumSquares = 0.0;
 
-                private Double stddev() {
-                    if (sampleSize < 1L) {
-                        return 0.0;
-                    }
-                    Double mean = sum / (double) sampleSize;
-                    Double variance = (sumSquares / (double) sampleSize) - (mean * mean);
-                    return Math.sqrt(variance);
-                }
-
-                @Override
-                public Double undo(final Double valueToUndo, Double aggregateValue) {
-                    sampleSize -= 1L;
-                    sum -= valueToUndo;
-                    sumSquares -= (valueToUndo * valueToUndo);
-                    return stddev();
-                }
-
-                @Override
-                public Double initialize() {
+            private Double stddev() {
+                if (sampleSize < 1L) {
                     return 0.0;
                 }
+                Double mean = sum / (double) sampleSize;
+                Double variance = (sumSquares / (double) sampleSize) - (mean * mean);
+                return Math.sqrt(variance);
+            }
 
-                @Override
-                public Double aggregate(final Double currentValue, final Double aggregateValue) {
-                    sampleSize += 1L;
-                    sum += currentValue;
-                    sumSquares += (currentValue * currentValue);
-                    return stddev();
-                }
+            @Override
+            public Double undo(final Double valueToUndo, Double aggregateValue) {
+                sampleSize -= 1L;
+                sum -= valueToUndo;
+                sumSquares -= (valueToUndo * valueToUndo);
+                return stddev();
+            }
 
-                @Override
-                public Double merge(final Double aggOne, final Double aggTwo) {
-                    return aggTwo;
-                }
+            @Override
+            public Double initialize() {
+                return 0.0;
+            }
+
+            @Override
+            public Double aggregate(final Double currentValue, final Double aggregateValue) {
+                sampleSize += 1L;
+                sum += currentValue;
+                sumSquares += (currentValue * currentValue);
+                return stddev();
+            }
+
+            @Override
+            public Double merge(final Double aggOne, final Double aggTwo) {
+                return aggTwo;
+            }
         };
     }
 }
